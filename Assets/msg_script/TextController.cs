082567c67b209bff9a43c9cb754abd4eb7df9bc1
koji <yasuhirokojimakoyonplete@gmail.com>
2015-03-15 00:24:18 +0900

    クリックでメッセージを表示できるようにしました。

---
+++ Assets/msg_script/TextController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+	public string[] scenarios;
+	public Text uiText;
+
+	int currentLine = 0;
+
+	// Use this for initialization
+	void Start () {
+		TextUpdate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentLine < scenarios.Length && Input.GetMouseButtonDown (0)) {
+			TextUpdate();
+		}
+	}
+
+	void TextUpdate (){
+		uiText.text = scenarios [currentLine];
+		currentLine ++;
+	}
+}
