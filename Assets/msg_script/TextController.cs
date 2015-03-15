4b09f06316aa63c89ed859dcc4e33efd1a3906b6
koji <yasuhirokojimakoyonplete@gmail.com>
2015-03-16 00:01:47 +0900

    UNITY_NG_01-1 Show String by one character. #fix

--- Assets/msg_script/TextController.cs
+++ Assets/msg_script/TextController.cs
@@ -4,10 +4,17 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour {
 	public string[] scenarios;
-	public Text uiText;
+	//public Text uiText;
+	[SerializeField] Text uiText;
+	[SerializeField][Range (0.001f,0.3f)]
+	float intervalForCharacterDisplay = 0.05f;
 
-	int currentLine = 0;
-
+	private int currentLine = 0;
+	private string currentText = string.Empty;//now string
+	private float timeUntilDisplay = 0;
+	private float timeElapsed = 1;
+	private int lastUpdateCharacter = -1;//display String Num
+	
 	// Use this for initialization
 	void Start () {
 		TextUpdate ();
@@ -18,10 +25,23 @@ public class TextController : MonoBehaviour {
 		if (currentLine < scenarios.Length && Input.GetMouseButtonDown (0)) {
 			TextUpdate();
 		}
+
+		int displayCharacterCount = (int)(Mathf.Clamp01 ((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
+
+		if (displayCharacterCount != lastUpdateCharacter) {
+			uiText.text = currentText.Substring(0,displayCharacterCount);
+			lastUpdateCharacter = displayCharacterCount;
+		}
 	}
 
 	void TextUpdate (){
-		uiText.text = scenarios [currentLine];
+		//uiText.text = scenarios [currentLine];
+		currentText = scenarios [currentLine];
 		currentLine ++;
+
+		timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+		timeElapsed = Time.time;
+
+		lastUpdateCharacter = -1;
 	}
 }
