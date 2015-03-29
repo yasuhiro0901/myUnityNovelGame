4a84fb3f3b7309216e616b31c64fac9ab3065080
yasuProCG <yproductcreates@gmail.com>
2015-03-29 14:36:18 +0900

    UNITY_NG_01-3  I was changed the character display.  #fix

---
+++ Assets/msg_script/CharaController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharaController : MonoBehaviour {
+	public RawImage centerChara;
+	public Texture[] charaImages;
+	private int charaImagesIndex;
+
+	// Use this for initialization
+	void Start () {
+		charaImagesIndex = 0;
+		centerChara.texture = charaImages [charaImagesIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			charaImagesIndex++;
+			centerChara.texture = charaImages [charaImagesIndex];
+		}
+	}
+}
