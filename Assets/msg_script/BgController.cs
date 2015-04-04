7859b313549415304907b4e0fa3bc4664dc17aa8
yasuProCG <yproductcreates@gmail.com>
2015-04-04 22:05:55 +0900

    UNITY_NG_01-4  I was changed the backgound image display.  #fix

---
+++ Assets/msg_script/BgController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BgController : MonoBehaviour {
+	public RawImage bg;
+	public Texture[] bgImages;
+	private int bgImagesIndex;
+	// Use this for initialization
+	void Start () {
+		bgImagesIndex = 0;
+		bg.texture = bgImages [bgImagesIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			bgImagesIndex++;
+			bg.texture = bgImages [bgImagesIndex];
+		}
+	}
+}
